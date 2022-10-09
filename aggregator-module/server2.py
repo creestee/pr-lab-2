3acc121a29f3818cf1b320bf27b35f07a236faa6
creestee <cristi.ccristi0@Gmail.com>
2022-10-09 23:51:46 +0300

    added aggregator service, modified in docker-compose file

---
+++ aggregator-module/server2.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, json
+from threading import Thread, Lock
+from components import *
+
+import time
+import queue
+import requests
+
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+THREADS = 5
+
+app = Flask(__name__)
+deposit = queue.Queue()
+
+@app.route('/deposit', methods=['POST'])
+def process_json():
+    deposit.put(json.loads(request.data))
+    return {'status_code': 200}
+
+class Delivery(Thread):
+    def __init__(self, delivery_id, *args, **kwargs):
+        self.delivery_id = delivery_id
+        super(Delivery, self).__init__(*args, **kwargs)
+
+    def deliver(self):
+        object = deposit.get()
+        object['status'] = 'delivered'
+
+        r = requests.post(
+            url='http://factory:8002/distribution',
+            json=object
+        )
+
+        print(f"Object : {object['object']}, Object_ID : {object['object_id']}, Status : {BCOLORS.OKGREEN}{object['status']}{BCOLORS.ENDC}")
+
+
+    def run(self):
+        while True:
+            self.deliver()
+            time.sleep(1)
+
+def run_aggregator_module():
+    threads = list()
+
+    main_thread = Thread(target=lambda: app.run(
+        host='0.0.0.0', 
+        port=8001))
+    
+    threads.append(main_thread)
+
+    for i in range(1, THREADS + 1):
+        delivery = Delivery(i)
+        threads.append(delivery)
+    
+    for i, thread in enumerate(threads):
+        thread.start()
+
+if __name__ == '__main__':
+    run_aggregator_module()
